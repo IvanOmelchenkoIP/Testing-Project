8cbaf057435ae4742c0c38c014a1cc6047468139
IvanOmelchenkoIP <io25112002@gmail.com>
2023-01-29 20:20:41 +0200

    Return view to a html page
    Return view to html page through REST controller

--- src/auth/src/main/java/com/testingproject/auth/controller/RegisterController.java
+++ src/auth/src/main/java/com/testingproject/auth/controller/RegisterController.java
@@ -3,13 +3,16 @@ package com.testingproject.auth.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/register")
 @RestController
 public class RegisterController {
 	
 	@GetMapping
-	public String registerPage() {
-		return "register";
+	public ModelAndView registerPage() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("register.html");
+		return mav;
 	}
 }
