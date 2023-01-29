8ccecb78a46d96e51b13c9eadf11a0f50d9fe69b
IvanOmelchenkoIP <io25112002@gmail.com>
2023-01-29 23:03:28 +0200

    Add proper submition
    Add proper submition of user data with js and acception POST request
    with Spring Boot

--- src/auth/src/main/java/com/testingproject/auth/controller/RegisterController.java
+++ src/auth/src/main/java/com/testingproject/auth/controller/RegisterController.java
@@ -1,7 +1,11 @@
 package com.testingproject.auth.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,4 +19,11 @@ public class RegisterController {
 		mav.setViewName("register.html");
 		return mav;
 	}
+	
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public void registerNewUser(@RequestParam("username") String name, @RequestParam("email") String email, @RequestParam("passwd") String passwd) {
+		System.out.println(name + " " + email + " " + passwd);
+	}
 }
+
+
--- src/auth/src/main/resources/static/js/register.js
+++ src/auth/src/main/resources/static/js/register.js
@@ -6,10 +6,28 @@ const RESIGSTRATION_FORMS = {
   passwd: "passwd",
 };
 
-const getFormContentsByName = (name) => document.getElementsByName(name)[0].value;
+const getFormContentsByName = (name) =>
+  document.getElementsByName(name)[0].value;
 
-const registerUser = () => {
+const postRequest = async (route, json) => {
+  await fetch(route, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(json),
+  })
+    .catch(console.error);
+};
+
+const registerUser = async () => {
   const username = getFormContentsByName(RESIGSTRATION_FORMS.username);
   const email = getFormContentsByName(RESIGSTRATION_FORMS.email);
   const passwd = getFormContentsByName(RESIGSTRATION_FORMS.passwd);
+  await postRequest("http://localhost:8080/register?username="+username+"&email="+email+"&passwd="+passwd, {
+    username: username,
+    email: email,
+    passwd: passwd,
+  });
 };
