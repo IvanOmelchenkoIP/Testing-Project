790b43f780ba261a15259f5f00c29b13f7981aa8
IvanOmelchenkoIP <io25112002@gmail.com>
2023-01-30 12:38:28 +0200

    Add home page
    Add routing for home page

---
+++ src/auth/src/main/java/com/testingproject/auth/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.testingproject.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("/")
+@RestController
+public class HomeController {
+
+	@GetMapping
+	public ModelAndView home() {
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("home.html");
+		return mav;
+	}
+}
