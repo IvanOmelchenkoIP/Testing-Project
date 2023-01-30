9f66cdd2e2f7b75095b2aee583daa80181a6ae4f
IvanOmelchenkoIP <io25112002@gmail.com>
2023-01-30 21:41:07 +0200

    Change js
    Change register.js for better readability

--- src/auth/src/main/resources/static/js/register.js
+++ src/auth/src/main/resources/static/js/register.js
@@ -6,28 +6,48 @@ const RESIGSTRATION_FORMS = {
   passwd: "passwd",
 };
 
+const jsonHeaders = {
+  Accept: "application/json",
+  "Content-Type": "application/json",
+};
+
+const METHODS = {
+  POST: "POST",
+};
+
+const fetchJson = async (method, json = null, route = window.location.href) => {
+  try {
+    const response = await fetch(route, {
+      method: method,
+      headers: jsonHeaders,
+      body: JSON.stringify(json),
+    });
+    return response.json();
+  } catch (err) {
+    throw err;
+  }
+};
+
 const getFormContentsByName = (name) =>
   document.getElementsByName(name)[0].value;
 
-const postRequest = async (route, json) => {
-  await fetch(route, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(json),
-  }).then(response => console.log(response.json()))
-    .catch(console.error);
-};
-
-const registerUser = async () => {
+const getUserData = () => {
   const username = getFormContentsByName(RESIGSTRATION_FORMS.username);
   const email = getFormContentsByName(RESIGSTRATION_FORMS.email);
   const passwd = getFormContentsByName(RESIGSTRATION_FORMS.passwd);
-  await postRequest("http://localhost:8080/register", {
-    username: username,
-    email: email,
-    passwd: passwd,
-  });
+  return { username, email, passwd };
+};
+
+const registerUser = () => {
+  const userData = getUserData();
+  try {
+    const response = fetchJson(
+      METHODS.POST,
+      userData,
+      "http://localhost:8080/register"
+    );
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
 };
