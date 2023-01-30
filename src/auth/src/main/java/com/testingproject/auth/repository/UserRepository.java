357b7f200838be1fb19c67677d2af0d9aa0f3fea
IvanOmelchenkoIP <io25112002@gmail.com>
2023-01-30 19:29:36 +0200

    Add select statements
    Add select statements to repository

--- src/auth/src/main/java/com/testingproject/auth/repository/UserRepository.java
+++ src/auth/src/main/java/com/testingproject/auth/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.testingproject.auth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.testingproject.auth.entity.User;
@@ -8,4 +10,9 @@ import com.testingproject.auth.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, String>{
 
+	@Query("SELECT * FROM users WHERE users.username = :username")
+	public User getByUsername(@Param("username") String username);
+	
+	@Query("SELECT * FROM users WHERE users.email = :email")
+	public User getByEmail(@Param("username") String email);
 }
