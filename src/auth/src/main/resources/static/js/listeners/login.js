f6358d64dc001d12da7c933b61a2bdd3feee8ba9
IvanOmelchenkoIP <io25112002@gmail.com>
2023-02-02 22:31:00 +0200

    Integrate userService with other methods
    Integrate userService with login and logout

--- src/auth/src/main/resources/static/js/listeners/login.js
+++ src/auth/src/main/resources/static/js/listeners/login.js
@@ -3,6 +3,6 @@
 import userService from "../auth-services/user.js";
 import { DOM_DATA_GETTERS } from "../utils/dom-parser.js";
 
-DOM_DATA_GETTERS.id("logout-user").addEventListener("click", () =>
-  console.log("logout")
-);
+DOM_DATA_GETTERS.id("logout-user").addEventListener("click", () => {
+  userService.logout();
+});
