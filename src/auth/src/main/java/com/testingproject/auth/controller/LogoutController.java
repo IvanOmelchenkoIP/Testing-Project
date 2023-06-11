4876920c2b578b09cff5b9f6deb13548b1675f96
IvanOmelchenkoIP <io25112002@gmail.com>
2023-06-11 22:00:15 +0300

    Add logout controller
    Add logout controller for future implementation

---
+++ src/auth/src/main/java/com/testingproject/auth/controller/LogoutController.java
@@ -0,0 +1,10 @@
+package com.testingproject.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/logout")
+@Controller
+public class LogoutController {
+
+}
