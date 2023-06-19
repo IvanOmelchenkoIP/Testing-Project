33471d68c81ab80c1d0ec378ee55c0f8db3c77a4
IvanOmelchenkoIP <io25112002@gmail.com>
2023-06-19 23:03:47 +0300

    Delete *.back and static
    Delete .=backup files of README and outdated static

--- src/auth/target/classes/static/js/listeners/login.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-import userService from "../auth-services/user.js";
-import { DOM_DATA_GETTERS } from "../utils/dom-parser.js";
-
-DOM_DATA_GETTERS.id("login-user").addEventListener("click", () => {
-  userService.logout();
-});
