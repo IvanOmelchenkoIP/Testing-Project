c73d3fc7f43c22f9057a7521505e1c8022130570
IvanOmelchenkoIP <io25112002@gmail.com>
2023-06-27 14:38:46 +0300

    Add js storages and implement cookies
    Add js storages and implement ways of working with cookies

---
+++ src/auth/src/main/resources/static/js/data/storage/session-storage-processor.js
@@ -0,0 +1,9 @@
+"use strict";
+
+class SessionStorageProcessor {
+	SessionStorageProcessor() {
+		throw new Error("Not implemented!");
+	}
+}
+
+export default SessionStorageProcessor;
---
+++ src/auth/src/main/resources/static/js/data/storage/storages.js
@@ -0,0 +1,7 @@
+"use strict";
+
+import CookiesProcessor from "./cookies-processor.js";
+
+const cookies = CookiesProcessor();
+
+export { cookies };
