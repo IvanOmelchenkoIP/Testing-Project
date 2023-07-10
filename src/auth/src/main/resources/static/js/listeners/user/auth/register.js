2a40476c826654b59d43b1f0872fd55e8082c40c
IvanOmelchenkoIP <io25112002@gmail.com>
2023-07-10 10:45:27 +0300

    Revert "Fix register"
    This reverts commit f57be046c91d725dac69a9aefde9e733697b0946.

--- src/auth/src/main/resources/static/js/listeners/user/auth/register.js
+++ src/auth/src/main/resources/static/js/listeners/user/auth/register.js
@@ -1,7 +1,7 @@
 "use strict";
 
 import { userAuthService } from "../../../services/services.js";
-import domSelector from "../../../dom/select/instances/dom-selector.js";
+import selector from "../dom/select/dom-selector.js";
 import authValidateMediator from "../../../mediators/user/auth-validate-mediator.js";
 import authResetErrorFieldsMediator from "../../../mediators/user/auth-reset-error-mediator.js";
 import deepCopy from "../../../utils/generic/deep-copy/deep-copy.js";
@@ -10,7 +10,7 @@ const FIELD_NAMES = ["username", "email", "passwd"];
 const RESET_FIELDS = deepCopy(FIELD_NAMES);
 RESET_FIELDS.push("error-field");
 
-domSelector.selectById("register-user").addEventListener("click", () => {
+selector.selectById("register-user").addEventListener("click", () => {
 	authResetErrorFieldsMediator(...RESET_FIELDS);
 	const { valid, data } = domSelector.selectAllByNameAndValidate(authValidateMediator, ...FIELD_NAMES);
 	if (valid) userAuthService.register(data);
