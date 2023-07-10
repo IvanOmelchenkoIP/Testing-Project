052369bb837c468f0f8e322561bfddc23a9977d2
IvanOmelchenkoIP <io25112002@gmail.com>
2023-07-10 12:46:53 +0300

    Implement logout controller and fixes
    Implement logout controller and clear some unneeded files

--- src/auth/src/main/java/com/testingproject/auth/controller/LogoutController.java
+++ src/auth/src/main/java/com/testingproject/auth/controller/LogoutController.java
@@ -1,16 +1,22 @@
 package com.testingproject.auth.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
-@RequestMapping("/logout")
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RequestMapping("/logoutUser")
 @Controller
 public class LogoutController {
 
 	@PostMapping
-	public RedirectView logoutUser() {
-		return new RedirectView("/");
+	public ResponseEntity<?> logoutUser(HttpServletRequest request) {
+		Cookie[] cookies = request.getCookies();
+		for (Cookie cookie : cookies) cookie.setMaxAge(0);
+		return ResponseEntity.ok().build();
 	}
 }
