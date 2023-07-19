d5ac15b49e6c8f2a6686d97087dacae634a9242f
IvanOmelchenkoIP <io25112002@gmail.com>
2023-07-19 16:44:55 +0300

    Add constructor
    Add simple constructor page

--- src/auth/src/main/java/com/testingproject/auth/controller/assignment/AssignmentConstructorController.java
+++ src/auth/src/main/java/com/testingproject/auth/controller/assignment/AssignmentConstructorController.java
@@ -1,5 +1,16 @@
 package com.testingproject.auth.controller.assignment;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("/assignmentConstructor")
+@RestController
 public class AssignmentConstructorController {
 
+	@GetMapping
+	public ModelAndView showAssignmentConstructor() {
+		return new ModelAndView("assignmentconstructor.html");
+	}
 }
