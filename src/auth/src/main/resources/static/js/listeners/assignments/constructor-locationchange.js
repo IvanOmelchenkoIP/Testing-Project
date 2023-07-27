357f50b30356392b553536d3ca453c9565e28d05
IvanOmelchenkoIP <io25112002@gmail.com>
2023-07-27 22:48:09 +0300

    Change page listener locations
    Change page listeners locations for a single file for easier location,
    not crowding html with many scripts which will also help with load times

--- src/auth/src/main/resources/static/js/listeners/assignments/constructor-locationchange.js
+++
@@ -1,5 +0,0 @@
-"use strict"
-
-import constructorOnloacationchangeHandler from "../../functions/listener-handlers/assignments/constructor-locationchange-handler.js";
-
-window.addEventListener("locationchange", constructorOnloacationchangeHandler);
--- src/auth/src/main/resources/static/js/listeners/assignments/page-state-listeners.js
+++ src/auth/src/main/resources/static/js/listeners/assignments/page-state-listeners.js
@@ -1,3 +1,14 @@
-/**
- * 
- */
+"use strict";
+
+import constructorOnloacationchangeHandler from "../../functions/listener-handlers/assignments/constructor-locationchange-handler.js";
+import constructorOnloadHandler from "../../functions/listener-handlers/assignments/constructor-onload-handler.js";
+
+window.addEventListener("locationchange", constructorOnloacationchangeHandler);
+
+(() => { 
+	if (document.readyState == "complete") {
+		constructorOnloadHandler();
+	} else {
+		window.addEventListener("load", constructorOnloadHandler);
+	}
+})();
