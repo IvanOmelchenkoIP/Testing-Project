8f5c883bdb80912bacb88b784db073f57e8ba4ba
IvanOmelchenkoIP <io25112002@gmail.com>
2023-09-03 15:45:33 +0300

    Implement listener handlers to add layers
    Implement listener handlers that will be adding new layers

--- src/auth/src/main/resources/static/js/functions/listener-handlers/assignments/constructor-onload-handler.js
+++ src/auth/src/main/resources/static/js/functions/listener-handlers/assignments/constructor-onload-handler.js
@@ -5,13 +5,17 @@ import SelectedState from "../../../context/assignments/implementations/selected
 import LayerOrderState from "../../../context/assignments/implementations/order-state.js";
 
 const constructorOnloadHandler = () => {
-	assignmentConstructorContext.selectedTask = new SelectedState();
+	assignmentConstructorContext.layers = {
+		selectedChapter: new SelectedState(),
+		selectedChildLayer: new SelectedState(),
+	};
 	assignmentConstructorContext.selectedAnswer = new SelectedState();
 	assignmentConstructorContext.selectedEditable = new SelectedState();
 	assignmentConstructorContext.namingOrders = {
 		question: new LayerOrderState(),
 		task: new LayerOrderState(),
 		chapter: new LayerOrderState(),
+		qpool: new LayerOrderState(),
 		variant: new LayerOrderState(),  
 	};
 	assignmentConstructorContext.addSelection = {
